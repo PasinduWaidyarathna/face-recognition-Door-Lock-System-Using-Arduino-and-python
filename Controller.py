302e971f2f0e4f1aadad040e4f9a436d1e3d8a5e
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2023-07-18 18:30:34 +0530

    Add files via upload

---
+++ Controller.py
@@ -0,0 +1,36 @@
+import pyfirmata
+
+comport = 'COM4'
+
+board = pyfirmata.Arduino(comport)
+
+led_1 = board.get_pin('d:2:o')
+led_2 = board.get_pin('d:3:o')
+led_3 = board.get_pin('d:4:o')
+buzzer_1 = board.get_pin('d:5:o')
+
+def doorlockCondition(value):
+    if value == 1:
+        led_2.write(1)
+        #led_3.write(0)
+    if value == 0:
+        led_2.write(0)
+       # led_3.write(1)
+def doorunlockCondition(value):
+    if value == 1:
+        #led_2.write(1)
+        led_3.write(1)
+    if value == 0:
+       # led_2.write(0)
+        led_3.write(0)
+def systemCondition(value):
+    if value == 1:
+        led_1.write(1)
+    #led_3.write(1)
+
+
+def buzzerON(value):
+    if value == 1:
+        buzzer_1.write(1)
+    if value == 0:
+        buzzer_1.write(0)
